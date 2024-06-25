fc8e187a72c60e3eb65b7026fe46cd1bf6a57022
Natdanai Baothong <hotgabpom@gmail.com>
2024-06-25 17:20:50 +0700

    test Commander

---
+++ 6.6 commander - การสร้าง CLI อย่างง่ายสำหรับการรับข้อมูลเมื่อใช้คำสั่งต่าง ๆ/index.js
@@ -0,0 +1,57 @@
+// console.log(process.argv);
+
+const { program } = require('commander');
+
+program
+  .name('simple-calc')
+  .description('A simple calculator for you')
+  .version('1.0.0');
+
+function addCommand(name, operater) {
+  program
+    .command(`${name} <x> <y>`)
+    .option('-f, --float <number>')
+    //   .requiredOption('<x>, --var-x <num>')
+    //   .requiredOption('<y>, --var-y <num>')
+    .action((x, y, opts) => {
+      x = +x;
+      y = +y;
+      if (Number.isNaN(x) || Number.isNaN(y)) {
+        throw new Error('some args is NaN');
+      }
+      let ans;
+      switch (operater) {
+        case '+':
+          ans = x + y;
+          break;
+
+        case '-':
+          ans = x - y;
+          break;
+
+        case '*':
+          ans = x * y;
+          break;
+
+        case '/':
+          ans = x / y;
+          break;
+        default:
+          break;
+      }
+      if (opts.float) {
+        ans = ans.toFixed(opts.float);
+      }
+      console.log(`Answer is ${ans}`);
+    });
+}
+
+addCommand('plus', '+');
+addCommand('minus', '-');
+addCommand('multiply', '*');
+addCommand('divide', '/');
+
+program.parse();
+
+const options = program.opts();
+// console.log(options);
